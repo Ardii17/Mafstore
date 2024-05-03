c4f30356fe48a94a13852c12d48f2033e65853d0
mardi <mardif45@gmail.com>
2024-05-03 20:19:44 +0700

    Alert System

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -2,16 +2,23 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAive43pRLgS3DEstoGjtVdFzgH1X8ptjw",
+  authDomain: "mafnext-1.firebaseapp.com",
+  projectId: "mafnext-1",
+  storageBucket: "mafnext-1.appspot.com",
+  messagingSenderId: "660610599939",
+  appId: "1:660610599939:web:039189cf063e394539d23",
 };
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
