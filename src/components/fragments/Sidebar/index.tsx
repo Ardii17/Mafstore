c2c8fb6a3d8dcdfcde4fa5f976a7c85b3695d116
mardi <mardif45@gmail.com>
2024-05-31 15:09:32 +0700

    Responsive Settings

--- src/components/fragments/Sidebar/index.tsx
+++ src/components/fragments/Sidebar/index.tsx
@@ -6,14 +6,15 @@ import { useRouter } from "next/router";
 import { useContext } from "react";
 
 type propTypes = {
-  className?: string
+  className?: string;
+  lists?: { title: string; icon: string; link: string }[];
   titleSidebar: string;
 };
 
 const SidebarLayout = (props: propTypes) => {
   const { pathname } = useRouter();
   const theme = useContext(ThemeContext);
-  const { titleSidebar, className } = props;
+  const { lists, titleSidebar, className } = props;
   return (
     <div
       className={`${className} sm:h-screen md:h-auto lg:h-screen md:w-full lg:w-1/5 max-sm:min-w-full md:border-b max-md:border-b`}
@@ -24,22 +25,39 @@ const SidebarLayout = (props: propTypes) => {
             {titleSidebar}
           </h1>
           <div className="flex max-sm:flex-col lg:flex-col gap-1 mt-4 ">
-            {theme?.listItems.map((list, index) => (
-              <Link
-                href={list.link}
-                key={index}
-                className={`flex items-center gap-4 cursor-pointer transition-all w-full max-sm:hover:bg-blue-900 lg:hover:bg-blue-900 py-2 px-3 rounded ${
-                  pathname === list.link ? "lg:bg-blue-900" : ""
-                }`}
-              >
-                <i
-                  className={`bx ${list.icon} text-2xl text-white md:text-black lg:text-white`}
-                />
-                <p className="text-white md:text-black lg:text-white">
-                  {list.title}
-                </p>
-              </Link>
-            ))}
+            {lists
+              ? lists.map((list, index) => (
+                  <Link
+                    href={list.link}
+                    key={index}
+                    className={`flex items-center gap-4 cursor-pointer transition-all w-full max-sm:hover:bg-blue-900 lg:hover:bg-blue-900 py-2 px-3 rounded ${
+                      pathname === list.link ? "lg:bg-blue-900" : ""
+                    }`}
+                  >
+                    <i
+                      className={`bx ${list.icon} text-2xl text-white md:text-black lg:text-white`}
+                    />
+                    <p className="text-white md:text-black lg:text-white">
+                      {list.title}
+                    </p>
+                  </Link>
+                ))
+              : theme?.listItems.map((list, index) => (
+                  <Link
+                    href={list.link}
+                    key={index}
+                    className={`flex items-center gap-4 cursor-pointer transition-all w-full max-sm:hover:bg-blue-900 lg:hover:bg-blue-900 py-2 px-3 rounded ${
+                      pathname === list.link ? "lg:bg-blue-900" : ""
+                    }`}
+                  >
+                    <i
+                      className={`bx ${list.icon} text-2xl text-white md:text-black lg:text-white`}
+                    />
+                    <p className="text-white md:text-black lg:text-white">
+                      {list.title}
+                    </p>
+                  </Link>
+                ))}
           </div>
         </div>
         <div className="w-full md:hidden lg:block my-4">
